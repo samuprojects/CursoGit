0fcf444484619359f199557567433283c3e8879d
Samuel <samuel@softblue.com.br>
2022-12-24 10:45:58 -0300

    Criada a classe empréstimo

--- src/biblioteca/BibliotecaApp.java
+++ src/biblioteca/BibliotecaApp.java
@@ -5,5 +5,7 @@ public class BibliotecaApp {
 	public static void main(String[] args) {
 		
 		Livro l = new Livro();
+		Usuario u = new Usuario();
+		Emprestimo emprestimo = new Emprestimo();
 	}
 }
